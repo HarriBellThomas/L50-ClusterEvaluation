c02d899bb98e4ebbe580c4b2c9533e9305cdefb3
Harri Bell-Thomas <harribt@live.co.uk>
2019-11-11 12:54:42 +0000

    Fixed 'ciphertext' error

--- init-remote.sh
+++ init-remote.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh L50@$1 -c '[ ! -d "L50-ClusterEvaluation" ] && git clone https://github.com/HarriBellThomas/L50-ClusterEvaluation.git; cd L50-ClusterEvaluation; git pull; ./init.sh'
+ssh L50@$1 '[ ! -d "L50-ClusterEvaluation" ] && git clone https://github.com/HarriBellThomas/L50-ClusterEvaluation.git; cd L50-ClusterEvaluation; git pull; ./init.sh'
