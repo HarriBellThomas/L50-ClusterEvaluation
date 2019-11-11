d72a73435b78dc0836d98c9960e96f8ef71121db
Harri Bell-Thomas <harribt@live.co.uk>
2019-11-11 16:12:24 +0000

    Fix printing rmeote stdout

--- experiments/remote_setup.py
+++ experiments/remote_setup.py
@@ -9,9 +9,10 @@ def run_remote_setup(exp_num, target, pswd):
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(str(target), username='L50', password=pswd)
-        cmd = "python3 {}/remote.py"
+        cmd = "python3 ~/L50-ClusterEvaluation/experiments/{}/remote.py"
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
-        print(ssh_stdout.read())
+        print("stdout:  " + str(ssh_stdout.read()))
+        print("stderr:  " + str(ssh_stderr.read()))
 
         print("Remote setup complete for experiment {}.\n".format(exp_num))
     else:
