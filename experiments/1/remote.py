e04b262ea19f5682608d15a8504760fc8bcd22c1
Harri Bell-Thomas <harribt@live.co.uk>
2019-11-12 01:33:12 +0000

    One more go at printing remote args

--- experiments/1/remote.py
+++ experiments/1/remote.py
@@ -4,6 +4,6 @@ import time
 import sys
 
 time = int(time.time())
-os.system("{} > ~/iperf-{}".format(str(sys.argv), time))
+os.system("'{}' > ~/iperf-{}".format(str(sys.argv), time))
 print("Running iperf...")
 os.system("iperf -s -i 1 -f m > ~/iperf-{}".format(time))
