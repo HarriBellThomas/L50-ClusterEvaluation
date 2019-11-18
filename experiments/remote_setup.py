5eb284bf62b9e1c90a374ee2bf5590d50c7c9bb2
Harri Bell-Thomas <harribt@live.co.uk>
2019-11-18 12:01:23 +0000

    Now using pkey for paramiko auth -- reset method too...

--- experiments/remote_setup.py
+++ experiments/remote_setup.py
@@ -33,12 +33,14 @@ def run_remote_setup(exp_num, target, pswd, args):
 def reset_remote(target, pswd):
     print("\nResetting evaluation environment for {}...".format(target))
     ssh = paramiko.SSHClient()
+    ssh_key = paramiko.RSAKey.from_private_key_file("/home/L50/.ssh/evaluation")
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(str(target), username='L50', password=pswd)
+    ssh.connect(str(target), username='L50', pkey=ssk_key)
     cmd = "tmux send -t evaluation C-c; sleep 2; "
     cmd = cmd + "tmux kill-session -t evaluation;"
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd, get_pty=True)
     print("stdout:  " + str(ssh_stdout.read()))
     print("stderr:  " + str(ssh_stderr.read()))
+    ssh.close()
 
     print("Done.\n")
