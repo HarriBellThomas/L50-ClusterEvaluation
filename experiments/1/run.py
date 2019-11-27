432f6dd571cb89c925c1b9a6b9a05d64f0698d9a
Harri Bell-Thomas <harribt@live.co.uk>
2019-11-27 23:05:21 +0000

    Write iperf results to file

--- experiments/1/run.py
+++ experiments/1/run.py
@@ -6,15 +6,15 @@ sys.path.insert(1, "{}/..".format(os.path.dirname(os.path.abspath(__file__))))
 import json
 from experiment import prepare_for_experiment
 
-def run(target, arguments):
+def run(target, arguments, results_dir):
     buffer_length = arguments.get('buffer_length', 80000)
     time = arguments.get('time', 5)
-    os.system("iperf -i 1 -t {} -f m -c {} -l {}".format(
-        time, str(target), buffer_length
+    os.system("iperf -i 1 -t {} -f m -c {} -l {} 2>&1 | tee {}/local".format(
+        time, str(target), buffer_length, results_dir
     ))
 
 
 if __name__ == "__main__":
     args = json.loads(sys.argv[2])
     results_dir = prepare_for_experiment(args, sys.argv[2], sys.argv[1])
-    run(sys.argv[1], args)
+    run(sys.argv[1], args, results_dir)
