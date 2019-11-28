514631243282aa353dced511e9596736290f89a0
Harri Bell-Thomas <harribt@live.co.uk>
2019-11-28 00:54:28 +0000

    Fixed remote hostname bug

--- experiments/1/remote.py
+++ experiments/1/remote.py
@@ -5,13 +5,16 @@ import os
 sys.path.insert(1, "{}/..".format(os.path.dirname(os.path.abspath(__file__))))
 import json
 import base64
+import socket
 from experiment import prepare_for_experiment
 
 time = int(time.time())
 argsEncodedBytes = base64.b64decode(sys.argv[1].encode("utf-8"))
 argsEncodedJson = str(argsEncodedBytes, "utf-8")
 args = json.loads(argsEncodedJson)
-results_dir = prepare_for_experiment(args, sys.argv[2], "(?)")
+
+ip = str(socket.gethostbyname(socket.gethostname()))
+results_dir = prepare_for_experiment(args, ip, "(?)")
 
 print("Running iperf...")
 os.system("iperf -s -i 1 -f m >> {}/remote".format(results_dir))
