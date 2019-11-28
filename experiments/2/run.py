4ccff47f8aabdf719ce4bf7efb894bdbbc41f838
Harri Bell-Thomas <harribt@live.co.uk>
2019-11-28 04:00:06 +0000

    Remove ping quiet mode and stopped it printing to the terminal

--- experiments/2/run.py
+++ experiments/2/run.py
@@ -13,7 +13,7 @@ def run(target, arguments, results_dir):
     duration = arguments.get('duration', 5)  # Run for x seconds.
 
     speed = "-f" if flood else "-i {}".format(interval)
-    os.system("sudo ping {} -q -s {} -w {} {} 2>&1 | tee {}/{}/{}/local".format(
+    os.system("sudo ping {} -s {} -w {} {} > {}/{}/{}/local".format(
         speed, size, duration, str(target), results_dir, arguments.get("_run"), str(target)
     ))
 
