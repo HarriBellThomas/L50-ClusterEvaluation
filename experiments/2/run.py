a4a5ccce81415ef455e2200c58fd10a38b824bbe
Harri Bell-Thomas <harribt@live.co.uk>
2019-11-28 03:10:41 +0000

    Updated ping experiment to log to both console and output

--- experiments/2/run.py
+++ experiments/2/run.py
@@ -13,7 +13,7 @@ def run(target, arguments, results_dir):
     duration = arguments.get('duration', 5)  # Run for x seconds.
 
     speed = "-f" if flood else "-i {}".format(interval)
-    os.system("sudo ping {} -s {} -w {} {} > {}/{}/{}/local".format(
+    os.system("sudo ping {} -s {} -w {} {} 2>&1 | tee {}/{}/{}/local".format(
         speed, size, duration, str(target), results_dir, arguments.get("_run"), str(target)
     ))
 
