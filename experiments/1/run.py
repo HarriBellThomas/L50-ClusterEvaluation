28025c03a8b99352c1e1c6e1ec1965e2159ff7f1
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-02 23:55:13 +0000

    Print iperf commands

--- experiments/1/run.py
+++ experiments/1/run.py
@@ -10,7 +10,7 @@ def run(target, arguments, results_dir):
     time = arguments.get('time', 5)
     udp = arguments.get('udp', False)
     bidirectional = arguments.get("bidir", False)
-    os.system("sudo iperf {} 2>&1 | tee {}/{}/{}/local".format(
+    command = "sudo iperf {} 2>&1 | tee {}/{}/{}/local".format(
         " ".join([
             "-u" if udp else "",
             "-i 0.5",
@@ -22,7 +22,9 @@ def run(target, arguments, results_dir):
             "-b 10g"
         ]),
         results_dir, arguments.get("_run"), str(target)
-    ))
+    )
+    print(command)
+    os.system(command)
 
 
 if __name__ == "__main__":
