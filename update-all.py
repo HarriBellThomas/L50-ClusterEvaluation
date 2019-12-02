7fc9f296e482f7e8710992545874208b10ccd122
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-02 17:19:25 +0000

    New update all script v3

--- update-all.py
+++ update-all.py
@@ -1,6 +1,7 @@
 import os
 hosts = os.environ["HOSTS"]
 if hosts:
+    os.system("cd ~/x; git pull")
     for host in hosts.split(","):
         os.system("ssh L50@{} 'cd ~/x; git pull' > /dev/null".format(host))
     print("Done")
