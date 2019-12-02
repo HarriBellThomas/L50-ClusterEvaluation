bd4b4861973d2320366ca32e4f98f136c3eca03e
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-02 23:51:58 +0000

    split out bandwidth from udp option

--- experiments/1/run.py
+++ experiments/1/run.py
@@ -12,13 +12,14 @@ def run(target, arguments, results_dir):
     bidirectional = arguments.get("bidir", False)
     os.system("sudo iperf {} 2>&1 | tee {}/{}/{}/local".format(
         " ".join([
-            "-u -b 1000m" if udp else "",
+            "-u" if udp else "",
             "-i 0.5",
             "-t {}".format(time),
             "-f m",
             "-l {}".format(buffer_length),
             "-c {}".format(str(target)),
-            "-d" if bidirectional else ""
+            "-d" if bidirectional else "",
+            "-b 10g"
         ]),
         results_dir, arguments.get("_run"), str(target)
     ))
