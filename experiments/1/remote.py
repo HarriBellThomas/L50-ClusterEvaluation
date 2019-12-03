315c039a08bfc0cc14c5a949408c3d3576007be8
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-03 01:03:28 +0000

    Remove iperf timeouts and do not use sudo

--- experiments/1/remote.py
+++ experiments/1/remote.py
@@ -11,7 +11,7 @@ import pathlib
 argsEncodedBytes = base64.b64decode(sys.argv[1].encode("utf-8"))
 argsEncodedJson = str(argsEncodedBytes, "utf-8")
 args = json.loads(argsEncodedJson)
-
+i
 path = pathlib.Path("/tmp/{}/{}".format(args.get("_id"), args.get("_run")))
 path.mkdir(parents=True, exist_ok=True)
 results_dir = path.absolute().as_posix()
@@ -19,7 +19,7 @@ results_dir = path.absolute().as_posix()
 udp = args.get("udp", False)
 
 print("Running iperf...")
-os.system("sudo iperf {} -s -i 0.5 -f m >> {}/remote".format(
+os.system("iperf {} -s -i 0.5 -f m >> {}/remote".format(
     "-u" if udp else "", 
     results_dir
 ))
--- experiments/1/run.py
+++ experiments/1/run.py
@@ -10,7 +10,7 @@ def run(target, arguments, results_dir):
     time = arguments.get('time', 5)
     udp = arguments.get('udp', False)
     bidirectional = arguments.get("bidir", False)
-    command = "timeout 25 sudo iperf {} 2>&1 | timeout 30 tee {}/{}/{}/local".format(
+    command = "iperf {} 2>&1 | tee {}/{}/{}/local".format(
         " ".join([
             "-u" if udp else "",
             "-i 0.5",
