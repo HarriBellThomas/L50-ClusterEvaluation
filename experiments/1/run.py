b32c493ecd06569ac6c17630d28458b65aaf462a
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-03 00:20:06 +0000

    Back to iperf2 -- now wuth backup timeout

--- experiments/1/run.py
+++ experiments/1/run.py
@@ -10,7 +10,7 @@ def run(target, arguments, results_dir):
     time = arguments.get('time', 5)
     udp = arguments.get('udp', False)
     bidirectional = arguments.get("bidir", False)
-    command = "sudo iperf3 {} 2>&1 | tee {}/{}/{}/local".format(
+    command = "timeout 25 sudo iperf {} 2>&1 | tee {}/{}/{}/local".format(
         " ".join([
             "-u" if udp else "",
             "-i 0.5",
@@ -18,7 +18,7 @@ def run(target, arguments, results_dir):
             "-f m",
             "-l {}".format(buffer_length),
             "-c {}".format(str(target)),
-            "--bidir" if bidirectional else "",
+            "-d" if bidirectional else "",
             "-b 10g"
         ]),
         results_dir, arguments.get("_run"), str(target)
