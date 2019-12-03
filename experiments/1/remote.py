7f248ee6fee8adedfbe839a6c35f1a4b255daece
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-03 01:22:56 +0000

    Fixed stray char in 1/remote.py

--- experiments/1/remote.py
+++ experiments/1/remote.py
@@ -11,7 +11,7 @@ import pathlib
 argsEncodedBytes = base64.b64decode(sys.argv[1].encode("utf-8"))
 argsEncodedJson = str(argsEncodedBytes, "utf-8")
 args = json.loads(argsEncodedJson)
-i
+
 path = pathlib.Path("/tmp/{}/{}".format(args.get("_id"), args.get("_run")))
 path.mkdir(parents=True, exist_ok=True)
 results_dir = path.absolute().as_posix()
