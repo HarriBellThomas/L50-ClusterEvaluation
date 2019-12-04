3d83361d6beaacff0b9d4ade3654c18020dd114a
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-04 00:20:27 +0000

    Exp 4 and 5 and writing remote init for simultaneous/concurrent v2

--- experiments/remote_setup.py
+++ experiments/remote_setup.py
@@ -9,6 +9,7 @@ import threading
 #
 def run_remote_setup(source, target, args, id, sleep=True):
     targets = target.split(",")
+    print(targets)
     if len(targets) > 0:
         threads = []
         for t in targets:
