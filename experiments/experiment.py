f174face959346a29fa98adf6c7a83a793b79f10
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-04 03:04:02 +0000

    Added crosstalk mode v2

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -19,9 +19,9 @@ import random
 from crosstalk import crosstalk, stop_crosstalk_both
 
 #
-def run_experiment(targets, definition, _id=str(uuid.uuid4()), crosstalk=False):
+def run_experiment(targets, definition, _id=str(uuid.uuid4()), cross=False):
     experiment_source = definition.get("src", definition.get("id", -1))
-    _id = "{}-crosstalk".format(_id) if crosstalk else _id
+    _id = "{}-crosstalk".format(_id) if cross else _id
     if validate_experiment(experiment_source):
         # Init experiment.
         exp_num = definition.get("id", -1)
@@ -72,8 +72,8 @@ def run_experiment(targets, definition, _id=str(uuid.uuid4()), crosstalk=False):
                 t = 0
                 target = random.sample(targets, 1)[0]
 
-                crosstalkers = random.sample([t for t in targets if t != target], 2)
-                if crosstalk:
+                crosstalkers = [] if not cross else random.sample([t for t in targets if t != target], 2)
+                if cross:
                     crosstalk(crosstalkers[0], crosstalkers[1], "1000m")
 
                 prepare_for_target(_id, i, target, definition, argument_sets[i])
@@ -83,15 +83,15 @@ def run_experiment(targets, definition, _id=str(uuid.uuid4()), crosstalk=False):
                 print("Argument set: {}".format(args))
                 run_in_mode(experiment_source, target, serialised_args, _id, i, results_dir, recipient, simultaneous, timeout)
                 
-                if crosstalk:
+                if cross:
                     stop_crosstalk_both(crosstalkers[0], crosstalkers[1])
                 print("")
 
             else:
                 for t in range(len(targets)):
                     target = targets[t]
-                    crosstalkers = random.sample([t for t in targets if t != target], 2)
-                    if crosstalk:
+                    crosstalkers = [] if not cross else random.sample([t for t in targets if t != target], 2)
+                    if cross:
                         crosstalk(crosstalkers[0], crosstalkers[1], "1000m")
 
                     prepare_for_target(_id, i, target, definition, argument_sets[i])
@@ -101,7 +101,7 @@ def run_experiment(targets, definition, _id=str(uuid.uuid4()), crosstalk=False):
                     print("Argument set: {}".format(args))
                     run_in_mode(experiment_source, target, serialised_args, _id, i, results_dir, recipient, simultaneous, timeout)
                     
-                    if crosstalk:
+                    if cross:
                         stop_crosstalk_both(crosstalkers[0], crosstalkers[1])
                     print("")
 
@@ -268,13 +268,13 @@ if __name__ == "__main__":
     if int(args.experiment) == 0:
         for experiment in experiment_data.keys():
             exp_definition = experiment_data.get(experiment, {})
-            crosstalk = exp_definition.get("crosstalk", False)
-            run_experiment(targets, exp_definition, "{}-experiment-{}".format(_id, experiment), crosstalk=crosstalk)
+            cross = exp_definition.get("crosstalk", False)
+            run_experiment(targets, exp_definition, "{}-experiment-{}".format(_id, experiment), cross=cross)
             time.sleep(1)
     else:
         exp_definition = experiment_data.get(int(args.experiment), {})
-        crosstalk = exp_definition.get("crosstalk", False)
-        run_experiment(targets, exp_definition, "{}-experiment-{}".format(_id, args.experiment), crosstalk=crosstalk)
+        cross = exp_definition.get("crosstalk", False)
+        run_experiment(targets, exp_definition, "{}-experiment-{}".format(_id, args.experiment), cross=cross)
     
     end = datetime.datetime.now().replace(microsecond=0)
     print("Duration: {}".format((end-start)))
