a4ecf01a23f338584052978c0ba6c8414b9c5fa8
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-05 03:13:39 +0000

    Set the buffer length for servers for exp 5 and 6 v29

--- experiments/5/run.py
+++ experiments/5/run.py
@@ -7,12 +7,12 @@ import json
 import time
 import uuid
 
-def start_server(target, arguments, results_dir):
+def start_server(target, arguments, results_dir, i):
     # udp = arguments.get("udp", False)
 
     print("Running iperf server...")
-    cmd = "timeout 20 iperf -s -i 0.5 -f m -p 51236 2>&1 | tee {}/{}/{}/local".format(
-        results_dir, arguments.get("_run"), str(target)
+    cmd = "timeout 20 iperf3 -s -i 0.5 -f m -p 51236 >> {}/{}/{}/local-{} &".format(
+        results_dir, arguments.get("_run"), str(target), i
     )
     os.system(cmd)
     # os.system("kill -9 $(pidof iperf3)")
@@ -20,4 +20,6 @@ def start_server(target, arguments, results_dir):
 
 if __name__ == "__main__":
     args = json.loads(sys.argv[2])
-    start_server(sys.argv[1], args, sys.argv[3])
+    for i in range(0, len(sys.argv[1].split(","))):
+        run_client(sys.argv[1], args, sys.argv[3], i)
+    time.sleep(20)
