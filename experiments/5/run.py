11ef8f054acd2c3bf1d5768692c25a8b8fc74263
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-05 01:34:35 +0000

    Set the buffer length for servers for exp 5 and 6 v10

--- experiments/5/run.py
+++ experiments/5/run.py
@@ -5,17 +5,22 @@ import os
 # sys.path.insert(1, "{}/..".format(os.path.dirname(os.path.abspath(__file__))))
 import json
 import time
+import uuid
 
 def start_server(target, arguments, results_dir):
     udp = arguments.get("udp", False)
 
     print("Running iperf server...")
-    cmd = "sudo iperf {} -s -i 0.5 -f m -p 51236 -l 1M -D -t 23 > {}/{}/{}/local".format(
+    cmd = "sudo iperf {} -s -i 0.5 -f m -p 51236 -l 1M -D -t 23 > {}/{}/{}/local ;".format(
         "-U -u" if udp else "", 
         results_dir, arguments.get("_run"), str(target)
     )
+    cmd = cmd + "sleep 23; sudo kill -9 $(pidof iperf); exit;"
+
+    _id = str(uuid.uuid4())
+    tmux_cmd = "tmux new-session -d -s evaluation-{};".format(_id)
+    tmux_cmd = tmux_cmd + "tmux send -t evaluation-{} \"{}\" ENTER; ".format(_id, cmd)
     os.system(cmd)
-    # os.system("(sleep 22s && sudo kill -9 $(pidof iperf)) &")
 
 
 if __name__ == "__main__":
