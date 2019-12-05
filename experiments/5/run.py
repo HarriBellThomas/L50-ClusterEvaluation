3e36b79b4bab570044762381c9ca7bceb109698d
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-05 01:43:34 +0000

    Set the buffer length for servers for exp 5 and 6 v14

--- experiments/5/run.py
+++ experiments/5/run.py
@@ -11,7 +11,7 @@ def start_server(target, arguments, results_dir):
     udp = arguments.get("udp", False)
 
     print("Running iperf server...")
-    cmd = "sudo iperf {} -s -i 0.5 -f m -p 51236 -l 1M -D -t 23 > {}/{}/{}/local".format(
+    cmd = "sudo iperf {} -s -f m -p 51236 -l 65000 -D -t 23 > {}/{}/{}/local".format(
         "-U -u" if udp else "", 
         results_dir, arguments.get("_run"), str(target)
     )
