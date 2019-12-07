879c79b0fd446ae63626590e5a7ba3d15873b5e7
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-07 21:43:09 +0000

    Try forcing pseudo-tty

--- init-remote.sh
+++ init-remote.sh
@@ -3,4 +3,4 @@
 cat ~/.ssh/evaluation.pub | ssh L50@$1 'cat >> ~/.ssh/authorized_keys && echo "Key copied"'
 scp -o StrictHostKeyChecking=no -i ~/.ssh/evaluation ~/.ssh/evaluation.pub L50@$1:~/.ssh/evaluation.pub
 scp -o StrictHostKeyChecking=no -i ~/.ssh/evaluation ~/.ssh/evaluation L50@$1:~/.ssh/evaluation
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/evaluation L50@$1 '[ ! -d "x" ] && git clone https://github.com/HarriBellThomas/L50-ClusterEvaluation.git x; cd x; git pull; ./init.sh'
+ssh -t -o StrictHostKeyChecking=no -i ~/.ssh/evaluation L50@$1 '[ ! -d "x" ] && git clone https://github.com/HarriBellThomas/L50-ClusterEvaluation.git x; cd x; git pull; ./init.sh'
