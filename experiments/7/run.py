e335fa7213fb303f84fe3faedcbcce42e0b03a71
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-07 22:12:20 +0000

    Iperf3 udp ingress

--- experiments/7/run.py
+++ experiments/7/run.py
@@ -1,3 +1,29 @@
+# #!/usr/bin/env python3
+# import argparse
+# import sys
+# import os
+# # sys.path.insert(1, "{}/..".format(os.path.dirname(os.path.abspath(__file__))))
+# import json
+# import time
+# import uuid
+
+# def start_server(target, arguments, results_dir):
+#     # udp = arguments.get("udp", False)
+
+#     print("Running tcpdump server...")
+#     cmd = "timeout 20 sudo tcpdump -i eth0 -nn -s0 -w {}/{}/{}/local.pcap udp &".format(
+#         results_dir, arguments.get("_run"), str(target)
+#     )
+#     os.system(cmd)
+#     # os.system("kill -9 $(pidof iperf3)")
+
+
+# if __name__ == "__main__":
+#     args = json.loads(sys.argv[2])
+#     start_server(sys.argv[1], args, sys.argv[3])
+#     time.sleep(20)
+
+
 #!/usr/bin/env python3
 import argparse
 import sys
@@ -7,12 +33,12 @@ import json
 import time
 import uuid
 
-def start_server(target, arguments, results_dir):
+def start_server(target, arguments, results_dir, i):
     # udp = arguments.get("udp", False)
 
-    print("Running tcpdump server...")
-    cmd = "timeout 20 sudo tcpdump -i eth0 -nn -s0 -w {}/{}/{}/local.pcap udp &".format(
-        results_dir, arguments.get("_run"), str(target)
+    print("Running iperf server...")
+    cmd = "timeout 20 iperf3 -s -i 0.5 -f m -p {} >> {}/{}/{}/local-{} &".format(
+        (51236 + i), results_dir, arguments.get("_run"), str(target), i
     )
     os.system(cmd)
     # os.system("kill -9 $(pidof iperf3)")
@@ -20,5 +46,6 @@ def start_server(target, arguments, results_dir):
 
 if __name__ == "__main__":
     args = json.loads(sys.argv[2])
-    start_server(sys.argv[1], args, sys.argv[3])
+    for i in range(0, len(sys.argv[1].split(","))):
+        start_server(sys.argv[1], args, sys.argv[3], i)
     time.sleep(20)
